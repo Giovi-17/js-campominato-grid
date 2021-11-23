eec81fbb09f727e8a76060ed005012b39bc126a6
Giovi-17 <giovanni.nuzzo17@gmail.com>
2021-11-23 17:16:15 +0100

    Seleziono un elemento e cambia colore di sfondo(solo il primo)

--- js/script.js
+++ js/script.js
@@ -7,3 +7,69 @@ con difficoltà 2 => tra 1 e 81
 con difficoltà 3 => tra 1 e 49
 Quando l'utente clicca su ogni cella, 
 la cella cliccata si colora di azzurro. */
+
+const easy = document.getElementById("easy");
+const play = document.getElementById("play");
+const nascosto = document.getElementById("nascosto");
+
+let diff = "easy";
+let numDiff;
+
+if(diff === 'easy'){
+    numDiff = 100;
+
+} else if(diff === 'normal'){
+    numDiff = 81;
+
+} else if(diff === 'hard'){
+    numDiff = 49;
+}
+
+const bigSquare = document.querySelector(".square-container");
+
+let squareDiff = dDiff(bigSquare, numDiff, diff);
+
+const selezionato = document.querySelector(".square");
+console.log(selezionato);
+
+//FUNZIONI
+
+// quando clicco su play compare la griglia
+play.addEventListener("click", 
+
+    function() {
+
+        nascosto.classList.remove("hidden");
+
+    }
+
+);
+
+//generatore di quadratini per ogni difficoltá
+function dDiff(bigSquare, numDiff, diff){
+
+    for(let i = 1; i <= numDiff; i++){
+
+        let newSquare = `
+    
+            <div class="col square square-${diff}"><a href="#">${i}</a></div>
+    
+        `;
+
+        bigSquare.innerHTML += newSquare;
+
+    }
+
+    
+}
+
+// quando clicco su una casella cambia colore di sfondo
+selezionato.addEventListener("click", 
+
+    function() {
+
+        selezionato.classList.add("selected");
+
+    }
+
+);
